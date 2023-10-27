90241e30696305ba211d01019407bdacfa383d28
rodglins <rodglins@gmail.com>
2023-10-26 22:30:40 -0300

    Enviando catalogo UAB Projeto Integrador

--- DjangoWebProject4/trash/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\DF195NL\source\repos\DjangoWebProject4\DjangoWebProject4\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
