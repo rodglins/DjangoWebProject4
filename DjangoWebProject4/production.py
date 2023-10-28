b57e9088df01dff858fcd2e283b97490445008be
rodglins <rodglins@gmail.com>
2023-10-28 09:33:08 -0300

    Enviando catalogo UAB Projeto Integrador 6

--- DjangoWebProject4/production.py
+++ DjangoWebProject4/production.py
@@ -6,8 +6,8 @@ from .database import DATABASES
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-#ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-#CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 
 # WhiteNoise configuration
